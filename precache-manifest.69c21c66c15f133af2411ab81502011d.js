35b6b863ce47a5281cb0f33d546bc571f2826a50
hemang05 <joshihemang05@gmail.com>
2020-07-13 23:20:44 +0530

    Updates

---
+++ precache-manifest.69c21c66c15f133af2411ab81502011d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af1d64a4cc6bc9f0f468ec4e72bce1b",
+    "url": "/hjworld/index.html"
+  },
+  {
+    "revision": "6891eae055014b7e29cb",
+    "url": "/hjworld/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "74095a142e158e55450d",
+    "url": "/hjworld/static/js/2.ecb7bbf4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hjworld/static/js/2.ecb7bbf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6891eae055014b7e29cb",
+    "url": "/hjworld/static/js/main.498cf2d6.chunk.js"
+  },
+  {
+    "revision": "d9e3c6851aa888e3d45b",
+    "url": "/hjworld/static/js/runtime-main.73619b5d.js"
+  },
+  {
+    "revision": "70388fc91f5c71d907e9f50fdb942700",
+    "url": "/hjworld/static/media/aps.70388fc9.png"
+  },
+  {
+    "revision": "42804525e30e49c01be95e60c76a68d7",
+    "url": "/hjworld/static/media/back2.42804525.jpg"
+  },
+  {
+    "revision": "fe62adbbdc695e62fe7decb5582ecd24",
+    "url": "/hjworld/static/media/iitr.fe62adbb.png"
+  },
+  {
+    "revision": "ec98271207e98e1be2d1505acb293fcb",
+    "url": "/hjworld/static/media/my_pic.ec982712.jpg"
+  },
+  {
+    "revision": "c26f4e12020c50c6fda3e8fa7899ea99",
+    "url": "/hjworld/static/media/pic2.c26f4e12.jpg"
+  },
+  {
+    "revision": "1c164d27ab1acd52ee9d04199b1fc4e9",
+    "url": "/hjworld/static/media/pic5.1c164d27.JPG"
+  },
+  {
+    "revision": "bd0d10fdffa407d7eb39dd382c152758",
+    "url": "/hjworld/static/media/resume.bd0d10fd.pdf"
+  },
+  {
+    "revision": "6b86ff6225b1b6ff599c981ee6a87370",
+    "url": "/hjworld/static/media/resume_1.6b86ff62.jpg"
+  }
+]);
